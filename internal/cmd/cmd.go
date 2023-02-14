37e168348540be1220770105936150a0ef17fd4f
Alastair Appleby <ali.appleby@live.co.uk>
2023-02-14 10:42:07 +0100

    Moved enablement of metrics and traces to independent requests (#14)

--- internal/cmd/cmd.go
+++ internal/cmd/cmd.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 
+	otelsdk "go.opentelemetry.io/otel"
+
 	"github.com/gigapipehq/loggen/internal/config"
 	"github.com/gigapipehq/loggen/internal/generators"
 	"github.com/gigapipehq/loggen/internal/otel"
+	"github.com/gigapipehq/loggen/internal/prom"
 	"github.com/gigapipehq/loggen/internal/senders"
 	_default "github.com/gigapipehq/loggen/internal/senders/default"
 )
@@ -15,8 +18,11 @@ type progressTracker interface {
 	Add(int)
 }
 
-func Do(cfg *config.Config, opName string, progress progressTracker) error {
-	ctx, span := otel.Tracer.Start(context.Background(), opName)
+func Do(ctx context.Context, cfg *config.Config, opName string, progress progressTracker) error {
+	shutdownMT := configureTracesAndTraces(ctx, cfg)
+	defer shutdownMT()
+
+	ctx, span := otel.Tracer.Start(ctx, opName)
 	defer span.End()
 
 	gen := func(ctx context.Context) senders.Generator {
@@ -48,4 +54,26 @@ func Do(cfg *config.Config, opName string, progress progressTracker) error {
 	defer cancel()
 	senders.Start(ctx, s, gen)
 	return nil
+}
+
+func configureTracesAndTraces(ctx context.Context, cfg *config.Config) func() {
+	if cfg.EnableMetrics {
+		prom.Initialize(ctx, cfg)
+	}
+	exporter := otel.NewNoopExporter()
+	if cfg.EnableTraces {
+		sender := _default.New().WithHeaders(map[string]string{
+			"X-API-Key":    cfg.APIKey,
+			"X-API-Secret": cfg.APISecret,
+		})
+		exporter = otel.NewExporter(cfg.URL, sender.Client())
+	}
+	tp := otel.NewProvider(exporter, cfg)
+	otelsdk.SetTracerProvider(tp)
+
+	return func() {
+		if tp != nil {
+			_ = tp.Shutdown(context.Background())
+		}
+	}
 }
